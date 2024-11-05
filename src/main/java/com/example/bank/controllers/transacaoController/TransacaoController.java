e8b8ab1f94e7c7fb0a940432ab90ff7a25013b08
Lucas Eneas <lucaseneasmarco@gmail.com>
2024-11-05 19:20:02 +0000

    First commit

---
+++ src/main/java/com/example/bank/controllers/transacaoController/TransacaoController.java
@@ -0,0 +1,35 @@
+package com.example.bank.controllers.transacaoController;
+
+import com.example.bank.domain.transacao.Transacao;
+import com.example.bank.domain.transacao.TransacaoRequest;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacaoController {
+    private List<Transacao> listaTransacoes =  new ArrayList<>();
+
+    @PostMapping
+    public ResponseEntity<Transacao> fazerTransacao(@Valid @RequestBody Transacao body){
+            listaTransacoes.add(body);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity listarTodasTransacoes(){
+        return ResponseEntity.ok(listaTransacoes);
+    }
+
+    @DeleteMapping
+    public ResponseEntity deletarTodasTrasacoes(){
+        listaTransacoes.clear();
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/example/bank/domain/transacao/Transacao.java
@@ -0,0 +1,21 @@
+package com.example.bank.domain.transacao;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Positive;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class Transacao {
+    @NotNull
+    @Positive
+    private Double valor;
+    @NotNull
+    @Past
+    private Date dataHora;
+}
